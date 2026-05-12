b74b4b777439d6e30ea3834ca3802f94d623e2c5
Jaime Hernández <jalheart@gmail.com>
2026-05-12 09:49:42 -0500

    feat: Update backend URL and add necessary permissions in AndroidManifest.xml

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
   static const String backendUrl =
-      'https://script.google.com/macros/s/AKfycbylsyZI7BfxLQP4GmCtonwolpt6QX5SbqQvEnKMSiNxz5f-f-jCmO8BULtwcpITQ2_q/exec';
+      'https://ucb0a54f0e6cabb292ccd43138f0.dl.dropboxusercontent.com/cd/0/inline/DAUaCuADhxWRp0g6y86i1tOYL4pFWvQOc8octGcIao6w7MWrZvupksxPRdajfWuZp4ucw9RkzzTqjAfSnqnW0oD8OSxTFSSRVI2C0_qZxU-SHy5VjrJ21zmBnVvv_NOjw3U/file#';
   static const String server = 'https://radios.miservidor.cloud';
 }
